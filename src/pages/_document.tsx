f39684cea8fcb7130ceed7fe10f6e4abb33e8336
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-01-22 11:36:20 -0300

    adicionei meta no head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+       <Head>
+        <meta name="description" content="UnlockingEnglish é um recurso online dedicado a ajudar os alunos a melhorar suas habilidades no idioma inglês." />
+      </Head>
       <body>
         <Main />
         <NextScript />
