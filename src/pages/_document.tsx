69250694bc323dad7bf9d158bdc5506cb91ce5a2
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-01-22 11:44:24 -0300

    atualizei descricao no meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="pt-br">
        <Head>
-        <meta name="description" content="UnlockingEnglish é um recurso online dedicado a ajudar os alunos a melhorar suas habilidades no idioma inglês." />
+        <meta name="description" content="UnlockingEnglish é um curso de aprendizado online que se dedica a aprimorar as habilidades em inglês dos alunos. Independente de você estar apenas começando sua jornada no aprendizado do inglês ou ser um profissional em busca de aperfeiçoamento, nosso curso foi projetado para atender a todos os níveis." />
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript /> 
       </body>
     </Html>
   );
