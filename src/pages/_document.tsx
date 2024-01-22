ec07142408c0be10cb97f82e0be6e96b15f28e0e
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2024-01-22 14:27:00 -0300

    mudei o meta na index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
        <Head>
-        <meta name="description" content="UnlockingEnglish é um curso de aprendizado online que se dedica a aprimorar as habilidades em inglês dos alunos." />
+        <meta name="description" content="UnlockingEnglish é um curso de aprendizado online que se dedica a aprimorar as habilidades em inglês dos alunos. Independente de você estar apenas começando sua jornada no aprendizado do inglês ou ser um profissional em busca de aperfeiçoamento, nosso curso foi projetado para atender a todos os níveis." />
       </Head>
       <body>
         <Main />
