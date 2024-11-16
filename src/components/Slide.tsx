d60f05cd997bb0bfdf4b3e4c766f252026f34748
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2024-11-16 16:06:27 -0300

    feat: add atualization

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -155,7 +155,7 @@ export function Slide() {
             id="slide-section"
             opacity={isVisible ? 1 : 0}
             transition="opacity 0.5s ease-in-out 0.2s"
-            marginTop={{ base: 30, sm: 90, md: 90, lg: 200, xl: 70 }}
+            marginTop={{ base: 0, sm: 90, md: 90, lg: 200, xl: 70 }}
             padding={{ base: "0 0px", sm: "0 20px", md: "0 30px", lg: "0 40px", xl: "0 50px" }} // Adiciona padding nas laterais
 
         >
