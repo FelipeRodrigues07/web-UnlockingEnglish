ffc7180d3389d8d66bcb5750ac889c51d702d779
FelipeRodrigues07 <feliperodriguesrs07@outlook.com>
2024-11-26 22:47:00 -0300

    feat: add atualization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,11 @@ export default function Document() {
   return (
     <Html lang="pt-br">
        <Head>
-        <meta name="description" content="Mega desconto especial de Blackfriday" />
-        <meta
+       <meta
           property="og:title"
           content="UnlockingEnglish - Curso de Inglês"
         />
-        <meta
-          property="og:image"
-          content="/logo2.jpeg" 
-        />
+        <meta name="description" content="Mega desconto especial de Blackfriday" />
       </Head>
       <body>
         <Main />
